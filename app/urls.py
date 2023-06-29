fab34175c5f0d654fee0b5a2b891214d810bd0fa
ErwinG90 <132687711+ErwinG90@users.noreply.github.com>
2023-06-29 18:49:30 -0400

    Add files via upload

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import registro
+from .views import *
 
 urlpatterns = [
     # Aqui iran las URLS para accesos a pantallas del proyecto
     path('registro/', registro, name='registro'),
+    path('', inicio, name='inicio'),
+    path('login/', inicio_sesion, name='inicio_sesion'),
+    path('ListadoClientes/',ListadoCliente, name="Listado"),
+    path('ModificarCliente/<rut>/',ModificarCliente, name="Modificar"),
+    path('EliminarCliente/<rut>/',EliminarCliente, name="Eliminar"),
+    
 ]
--- app/views.py
+++ app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
-from .forms import RegistroForm
-from . import models
+from .forms import *
+from .models import *
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required,permission_required
 
 # Create your views here.
 
 
 #Vista de registro de cuenta de usuario
+#PRIMERA REGLA DE ING: SI FUNCIONA NO LO TOQUEN
 def registro(request):
     
     if request.method == 'POST':
@@ -87,3 +89,66 @@ def registro(request):
         'data': {}
     }
     return render(request, 'app/registro/registro.html', context)
+
+
+def inicio_sesion(request):
+    data = {
+        'msg': ''
+    }
+    if request.method == 'POST':
+        
+        usuario = request.POST['usuario']
+        password = request.POST['password']
+        
+        usr = authenticate(username=usuario, password=password)
+        
+        if usr is not None:
+            login(request, usr)
+            return redirect(to=inicio)
+        
+        else:
+            data = {
+                'msg': 'Usuario y/o Contraseña Invalidos'
+            }
+    
+    return render(request, 'app/registro/login.html', data)
+
+def inicio(request):
+    return render(request, 'app/index.html')
+
+@permission_required('app.add_cliente')
+def ListadoCliente(request):
+    clientes = Cliente.objects.all()
+    data = {
+            'Clientes':clientes
+    }
+    return render(request, 'app/lista_cliente.html',data)
+
+@permission_required('app.add_cliente')
+def ModificarCliente(request,rut):
+    cliente = Cliente.objects.get(rut=rut)
+    data = {
+        'form':ClienteForm(instance=cliente)
+    }
+
+    if request.method == 'POST':
+        formulario = ClienteForm(data=request.POST, instance=cliente)
+        if formulario.is_valid():
+            formulario.save()
+            data['mensaje'] = "Modificado correctamente"
+            data['form'] = formulario
+    return render(request, 'app/Modificar_Cuenta.html',data)
+
+@permission_required('app.add_cliente')
+def EliminarCliente(request,rut):
+    cliente = Cliente.objects.get(rut=rut)
+    data = {
+        'form':ClienteForm(instance=cliente)
+    }
+
+    if request.method == 'POST':
+        formulario = ClienteForm(data=request.POST, instance=cliente)
+        if formulario.is_valid():
+            cliente.delete()
+    
+    return render(request, 'app/confirmacion.html',data)
